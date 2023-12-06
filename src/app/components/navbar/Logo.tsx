58c21664409b93538a69fc5505f52449da7a6ca5
Loc Nguyen Janeto <nguyentanloc@teams.airlab.dev>
2023-12-06 17:25:32 +0700

    add custom for airbnb

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,19 @@
+'use client';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      className=""
+      src="/images/logo.png"
+      alt="logo"
+      width="100"
+      height="100"
+    />
+  )
+}
+
+export default Logo
---
+++ src/app/components/navbar/Search.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
